19f9516403cf9830feb6f2ffff76245a60538678
Seven-sc <xuweijia627@gmail.com>
2024-12-07 23:26:09 +0800

    JSX语法

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,10 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
+  const name = "张飞";
+  const titleStyle = {color: "red", fontSize: 30};
+  const title1 = <span style={titleStyle}>hello world</span>
+  const list = ['赵云','关羽','刘备'];
   return (
     <>
       <div>
@@ -17,6 +21,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      {title1}
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -27,7 +32,11 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
+        {name}
       </p>
+      {list.map(item => {
+        return <span key={item}>{item} </span>;
+      })}
     </>
   )
 }
