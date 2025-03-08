5b636ab8068d9c70254caf9da82f673d8408bdbf
Seven-sc <xuweijia627@gmail.com>
2025-03-09 00:18:54 +0800

    函数定义

--- src/tsdemo/tstest.ts
+++ src/tsdemo/tstest.ts
@@ -7,4 +7,17 @@ const isMan:boolean = true
 const user:object = {}
 
 const big:bigint = 100n
+
+const arr: number[] = [1,2,3]
+
+const arr1: Array<Number> = [4,5,6]
+
+// 函数定义
+function add1(x:number, y:number): number {
+    return x + y;
+}
+
+const add2 = (a: number, b: number): number => {
+    return a + b;
+}
 export default {}
