b6b12e8b47d6de994207303e526b45a4d4f3676a
Seven-sc <xuweijia627@gmail.com>
2025-03-16 00:16:36 +0800

    BrowserRouter

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,20 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { BrowserRouter, Route, Router, Routes } from 'react-router-dom'
 
+function ReactDemo() {
+  return <h2>哈哈哈哈</h2>
+}
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element = {<App />}></Route>
+      <Route path='/react' element = { <ReactDemo /> }></Route>
+    </Routes> 
+  </BrowserRouter>
+
+  // <StrictMode>
+  //   <App />
+  // </StrictMode>,
 )
